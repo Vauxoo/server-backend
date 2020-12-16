de2e38c9b2f200f730aa20e6ccc1f743ea961169
Moises Lopez (moylop260) moylop260@vauxoo.com <moylop260@vauxoo.com>
2020-12-16 05:25:09 +0000

    fixup! fixup! manifest

--- base_speedup_translate/__manifest__.py
+++ base_speedup_translate/__manifest__.py
@@ -1,7 +1,7 @@
 # Copyright 2020 Vauxoo, S.A. de C.V.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 {
-    'name': 'Base Speedup Translate,
+    'name': 'Base Speedup Translate',
     'summary': 'Speedup translation of fields',
     "version": "12.0.1.0.0",
     "development_status": "Production/Stable",
